14ad80989ea85c943481dea02a4aea305b514df0
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-07-05 18:49:54 +0100

    Add py.typed file

--- kasai/__init__.py
+++ kasai/__init__.py
@@ -29,7 +29,7 @@
 from __future__ import annotations
 
 __productname__ = "hikari-kasai"
-__version__ = "0.4a"
+__version__ = "0.4a1"
 __description__ = "A bridge between Discord and Twitch chat."
 __url__ = "https://github.com/parafoxia/hikari-kasai"
 __docs__ = "https://hikari-kasai.readthedocs.io"

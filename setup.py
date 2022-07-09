d47ba3f1c852dfa0a6b26394dfa14ad053bd0606
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-07-09 17:29:57 +0100

    Switch from pre-alpha to alpha

--- setup.py
+++ setup.py
@@ -76,8 +76,8 @@ setuptools.setup(
     license=attrs["license"],
     classifiers=[
         # "Development Status :: 1 - Planning",
-        "Development Status :: 2 - Pre-Alpha",
-        # "Development Status :: 3 - Alpha",
+        # "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         # "Development Status :: 4 - Beta",
         # "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
